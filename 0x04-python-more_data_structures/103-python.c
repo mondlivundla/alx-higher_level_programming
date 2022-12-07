790ebb6d2d3b1e08f26ff38af2c55ddb144cf087
mondlivundla <mondlivundla7@gmail.com>
2022-12-07 22:59:08 +0200

    0x03-python-data_structures

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -40,6 +40,7 @@ void print_python_bytes(PyObject *p)
 
 	printf("\n");
 }
+
 /**
  * print_python_list - Prints list information
  *
@@ -66,3 +67,4 @@ void print_python_list(PyObject *p)
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
 	}
+}
