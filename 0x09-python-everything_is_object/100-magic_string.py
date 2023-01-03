f9dd655d95c4f9defa19d9f32e8fd574cd0f2f91
mondlivundla <mondlivundla7@gmail.com>
2023-01-03 12:42:20 +0200

    0x09-python-everything_is_object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
+def magic_string(iteration):
+	return "BestSchool, " * iteration
 
-
-def call_counter(func):
-    def helper():
-        helper.calls += 1
-        return func()
-    helper.calls = 0
-
-    return helper
-
-
-@call_counter
-def magic_string():
-    return 'BestSchool, ' * (magic_string.calls - 1) + 'BestSchool'
+for i in range(10):
+	print(magic_string(i))
