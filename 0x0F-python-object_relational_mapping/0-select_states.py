651b71756540d1398ba50a2276bf46b3397d5b64
mondlivundla <mondlivundla7@gmail.com>
2023-03-17 14:27:59 +0200

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
+"""
+This script should list all states from the
+database `hbtn_0e_0_usa`.
+"""
+
 import MySQLdb
-import sys
+from sys import argv
 
-# Get MySQL login credentials from command line arguments
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-# Connect to MySQL server running on localhost at port 3306
-db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                     passwd=mysql_password, db=database_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
 
-# create a cursor object to execute queries
-cursor = db.cursor()
-
-# Execute the query to get all states from the database and sort id
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-# Fetch all the rows and print them
-for row in cursor.fetchall():
-    print(row)
-
-# Close the cursor and database connection
-cursor.close()
-db.close()
+    for row in rows:
+        print(row)
